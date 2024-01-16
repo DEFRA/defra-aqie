22a5445d8802ed78809ad846b5dc7fca7621e861
ulysses <ulysses.alvarez@cognizant.com>
2024-01-16 09:41:27 +0000

    fixed title for homepage in header and clean some code.

--- server/controllers/home-controller.ts
+++ server/controllers/home-controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 
 function homepage(req: Request, res: Response, next: NextFunction) {
-  res.render('layout.njk', { serviceName: '' });
+  res.render('layout.njk', {
+    serviceName: 'Check local air quality',
+    page: 'homepage',
+  });
 }
 
 function login(req: Request, res: Response, next: NextFunction) {
-  res.render('login.njk', { serviceName: 'Check local air quality' });
+  res.render('login.njk', { serviceName: '' });
 }
 
 export default { homepage, login };
